5e0294fadddf88930e49350bfe156dbad3b61a3b
Austin Erlandson <austin@erlandson.com>
2015-06-15 15:56:50 -0500

    Setup guard, rspec, capybara, etc...

--- Gemfile
+++ Gemfile
@@ -1,10 +1,46 @@
 source 'https://rubygems.org'
+ruby '2.2.2'
 
-gem 'rails', '4.2.0'
-gem 'sass-rails', '~> 5.0'
-gem 'uglifier', '>= 1.3.0'
+
+gem 'bitters'
 gem 'coffee-rails', '~> 4.1.0'
+gem 'haml-rails'
+gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
+gem 'neat'
+gem 'pg' # For heroku
+gem 'rails', '4.2.1'
+gem 'redcarpet'
+gem 'sass-rails', '~> 5.0'
+gem 'simple_form'
+gem 'sorcery'
 gem 'turbolinks'
-gem 'sqlite3', group: :development
-gem 'pg', group: :production # For heroku
+gem 'uglifier', '>= 1.3.0'
+
+group :development do
+  gem 'refills'
+end
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+  gem 'fabrication'
+  gem 'faker'
+  gem 'rspec-rails'
+
+  # Guard is a watcher for your test files. Super cool...
+  gem 'guard-rspec', require: false
+
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'shoulda-matchers'
+end
---
+++ spec/features/user_views_posts_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+feature "User views posts" do
+
+  let(:julie){ Fabricate(:user, name: "Julie") }
+
+  background do
+    bob = Fabricate(:user, name: "Bob")
+    ingrid = Fabricate(:user, name: "Ingrid")
+    Fabricate(:post, author: bob, title: "Bob's Burger Recipe")
+    Fabricate(:post, author: julie, title: "Julie's Intro to XPath", body: "XPath is *great*")
+    Fabricate(:post, author: julie, title: "Julie's Over XPath", body: "XPath is *great*")
+    visit root_path
+  end
+
+  scenario "viewing the list of blogs" do
+    page.should have_content("Our Knowledgeable Contributors")
+    page.should have_link("Bob's Knowledge")
+    page.should have_link("Ingrid's Knowledge")
+    page.should have_link("Julie's Knowledge")
+  end
+
+  scenario "viewing the blog of a particular author" do
+    click_on "Julie's Knowledge"
+    page.should have_link("Julie's Intro to XPath")
+    page.should have_link("Julie's Over XPath")
+    page.should_not have_content("Bob's Burger Recipe")
+    page.should have_link("All Contributors", href: root_path)
+  end
+
+  scenario "viewing an individual post, rendered in markdown" do
+    click_on "Julie's Knowledge"
+    click_on("Julie's Intro to XPath")
+    page.should have_css("h1", text: "Julie's Intro to XPath")
+    page.should have_content("XPath is great")
+    page.should have_css("em", text: "great")
+    page.should have_css(".author", text: "Julie")
+    page.should have_link("Return to Julie's Knowledge", href: user_posts_path(julie))
+  end
+end
